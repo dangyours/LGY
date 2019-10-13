92eac100c1eecc59a89a5978a475d4fd16a729ce
Danyours <dangyours@gmail.com>
2019-10-13 13:57:28 +0800

    first

---
+++ config.js
@@ -0,0 +1,29 @@
+// 霸都丶傲天 2019年10月10日 https://github.com/AJLoveChina/birthday
+var config = {
+    // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
+    // 每句话尽量不要超过15个字,不然展示效果可能不太好
+    texts: [
+        "送给我",
+        "心爱的小可爱",
+        "今天是你的生日",
+        "这是我们在一起的",
+        "第二个生日了哦",
+        "去年的生日",
+        "还记得在王婆串串吃的呢",
+        "今年要吃好的喽哦",
+        "要把我家可爱猪猪喂饱饱",
+        "然后抱走",
+        "YAMI~~"
+    ],
+    // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
+    desc: {
+        turn_on: "点我",
+        play: "music~",
+        bannar_coming: "爱心",
+        balloons_flying: "好像少点东西",
+        cake_fadein: "蛋糕？",
+        light_candle: "蜡烛？",
+        wish_message: "生日快乐",
+        story: "A MESSAGE FOR YOU",
+    }
+};
